f0e47b091ff88576c4bb7b6f1c1d7dad2bb1e035
annguo1106 <annguo1106@gmail.com>
2024-10-14 21:38:58 +0800

    tmp

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ import os
 load_dotenv()
 GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+MONGO_URL=os.getenv("MONGO_URL")
+MONGO_NAME=os.getenv("MONGO_NAME")
 genai.configure(api_key=GEMINI_API_KEY)
 
 app = FastAPI()
