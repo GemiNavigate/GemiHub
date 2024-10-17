6c2b14ee6901cd1a6295444b6b163966cc458684
Kent <leekent173@gmail.com>
2024-10-17 10:36:21 +0800

    corpus agent workin

--- backend/src/Chat.py
+++ backend/src/Chat.py
@@ -7,13 +7,7 @@ import os
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE)
-scoped_credentials = credentials.with_scopes(
-    ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/generative-language.retriever'])
-generative_service_client = glm.GenerativeServiceClient(credentials=scoped_credentials)
-retriever_service_client = glm.RetrieverServiceClient(credentials=scoped_credentials)
-permission_service_client = glm.PermissionServiceClient(credentials=scoped_credentials)
+
 
 
 genai.configure(api_key=GEMINI_API_KEY)
