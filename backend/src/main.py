986ae895a293e8889e3faa9f05dd239f8e8479e7
benson0402 <benson930402@gmail.com>
2024-10-19 09:36:20 +0800

    Initial api interface

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,10 +3,6 @@ from pydantic import BaseModel, Field, constr
 from typing import List, Optional
 from datetime import datetime
 import logfire
-
-from Chat import ChatHandler
-
-app = FastAPI()
     
 class Filter(BaseModel):
     min_lat: float = Field(..., ge=-90.0, le=90.0)
@@ -16,38 +12,46 @@ class Filter(BaseModel):
     current_time: datetime
     time_range: int = 60 # In minute
 
+
 class Reference(BaseModel):
     messaege: str
     lat: float = Field(..., ge=-90.0, le=90.0)
     lng: float = Field(..., ge=-180.0, le=180.0)
     time: datetime
 
+
 class AskMessage(BaseModel):
     query: str
     filter: Filter
+
 
 class AskResponse(BaseModel):
     response: str
     reference: List[Reference]
 
+
 class ShareMessage(BaseModel):
+    tmp: str
     #  
 
 
 class ShareResponse(BaseModel):
+    tmp: str
     # 
-
-
-
 
 
 ########## API ###########
 
-@app.post("/ask")
-async def ask(ask_message: AskMessage):
-    return {"message":"OK"}
+app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "OK"}
 
+@app.post("/ask")
+async def ask(ask_message: AskMessage):
+    return {"message":"OK"}
+
+@app.post("/share")
+async def share(share_message: ShareMessage):
+    return {"message":"OK"}
