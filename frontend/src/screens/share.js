8e27f02ac239dacae916464d37eed329bedf5f34
hyching21 <sheep128763@gmail.com>
2024-10-20 10:55:34 +0800

    modify share mapview

--- frontend/src/screens/share.js
+++ frontend/src/screens/share.js
@@ -7,7 +7,107 @@ import GetLocation from 'react-native-get-location';
 import { TokenContext, LocationContext } from './Context';
 
 const { width, height } = Dimensions.get('window');
-
+const mapstyle = [
+    {
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#abbaa4"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#3f518c"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "color": "#ad9b8d"
+            }
+        ]
+    }
+]
 const styles = StyleSheet.create({
     container: {
         ...StyleSheet.absoluteFillObject,
@@ -339,6 +439,7 @@ export default function ShareScreen({}) {
                 ref={mapRef}
                 provider={PROVIDER_GOOGLE}
                 style={styles.map}
+                customMapStyle={mapstyle}
                 region={currentLocation ? {
                     latitude: currentLocation.latitude,
                     longitude: currentLocation.longitude,
